4e1d10aa2c8b968a92cf98540d54940829b23e90
rash <orashusedmund@gmail.com>
2023-04-04 18:20:10 +0100

    implementing my hocs

--- src/components/AuthGaurd.js
+++ src/components/AuthGaurd.js
@@ -1,34 +1,38 @@
 /* eslint-disable */
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { getCurrentUser } from "../api/authentication";
+import { MyContext } from "../context/MyContext";
 
-const logIn = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve({firstName: "Gaston", lastName: "Che"}), 2000);
-  });
-}
+// const logIn = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve({firstName: "Gaston", lastName: "Che"}), 2000);
+//   });
+// }
 
 const AuthGaurd = (Component) => {
   return function Gaurd(props) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [user, setUser] = useState();
+    const { customAlert, setLoadingAnime } = useContext(MyContext)
+    const [currentUser, setCurrentUser] = useState();
     const navigate = useNavigate();
 
     useEffect(() => {
-      setIsLoading(true);
-      logIn()
-        .then(setUser)
+      setLoadingAnime({message: 'Loadding user...', show: true})
+      getCurrentUser()
+        .then((res)=> {
+          console.log('this res', res)
+          setCurrentUser(res);
+        })
         .catch(() => {
-          console.log("user not logged in");
+          customAlert("User not logged in");
           navigate("/login", {replace: true});
         })
-        .finally(() => setIsLoading(false));
+        .finally(() => 
+        setLoadingAnime({message: '', show: false}));
     }, []);
 
-    if(isLoading) return <>loading user...</>;
-
-    return user ? <Component {...props} user={user} /> : <>redirecting...</>;
+    return <Component {...props} currentUser={currentUser} />;
   };
 };
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,21 +6,21 @@ import Navbar from '../components/Navbar';
 import Body from '../components/Body';
 import Footer from '../components/Footer';
 import AuthGaurd from '../components/AuthGaurd';
-import { MyContext } from '../context/MyContext';
+// import { MyContext } from '../context/MyContext';
 
-function Main({ name, user }) {
-  const { setCurrentUser } = React.useContext(MyContext);
+function Main({ name, currentUser }) {
+  // const { setCurrentUser } = React.useContext(MyContext);
 
-  React.useEffect(() => {
-    // causing a rerender so that the current user will be properly loaded
-    setCurrentUser(JSON.parse(localStorage.getItem('currentUser')));
-  }, []);
+  // React.useEffect(() => {
+  //   // causing a rerender so that the current user will be properly loaded
+  //   setCurrentUser(JSON.parse(localStorage.getItem('currentUser')));
+  // }, []);
 
   return (
     <div className="Main">
       <Navbar />
       <h1>
-        {name}-{user.firstName}
+        {name}-{currentUser?.first_name}
       </h1>
       <Body />
       <Footer />
